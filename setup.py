a5d3be6d10a48578319fb9334a877b57b8c693e9
Felix Hoffmann <uumqm@student.kit.edu>
2025-08-14 11:45:51 +0200

    Status Quo

--- setup.py
+++ setup.py
@@ -47,15 +47,15 @@ with open('README.md', 'r') as file:
 # 6. Make sure your license tag matches the LICENSE (and maybe LICENSE.LESSER) file distributed
 #    with your package (default: GNU Lesser General Public License v3)
 setup(
-    name='qudi-addon-template',  # Choose a custom name
+    name='qudi-nvCenter-modules',  # Choose a custom name
     version=version,  # Automatically deduced from "VERSION" file (see above)
     packages=find_namespace_packages(where='src'),  # This should be enough for 95% of the use-cases
     package_dir={'': 'src'},  # same
     package_data={'': []},  # include data files
-    description='A template package for qudi addons.',  # Meaningful short(!) description
+    description='A qudi addon for a NV center quantum optimal control suite.',  # Meaningful short(!) description
     long_description=long_description,  # Detailed description is taken from "README.md" file
     long_description_content_type='text/markdown',  # Content type of "README.md" file
-    url='https://github.com/Ulm-IQO/qudi-addon-template',  # URL pointing to your project page
+    url='https://github.com/Ulm-IQO/qudi-nvCenter-modules',  # URL pointing to your project page
     keywords=['qudi',             # Add tags here to be easier found by searches, e.g. on PyPI
               'experiment',
               'measurement',
@@ -64,7 +64,8 @@ setup(
               'laboratory',
               'instrumentation',
               'instrument',
-              'modular'
+              'modular',
+              'nvCenter',
               ],
     license='LGPLv3',  # License tag
     install_requires=windows_dep if sys.platform == 'win32' else unix_dep,  # package dependencies
